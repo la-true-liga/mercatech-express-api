87c6e33297607a37c6c135a4773f5abe2e7a2121
jdomingu98 <jadominguezarcas@gmail.com>
2023-11-10 19:17:03 +0100

    added methods validation

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -6,7 +6,6 @@ const router: Router = Router();
 
 router.post('/', itemController.createItem);
 router.get('/', itemController.getItems);
-router.get('/:id', itemController.getItemById);
 router.put('/:id', itemController.updateItem);
 router.delete('/:id', itemController.deleteItem);
 
